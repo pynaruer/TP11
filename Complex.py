76bb01b99b6d21326a947d1a9c09a551a7023148
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-03-24 11:57:32 +0100

    tests

--- Complex.py
+++ Complex.py
@@ -3,6 +3,12 @@ class Complex:
         self.__re = re
         self.__im = im
 
+    def getRe(self):
+        return self.__re
+
+    def getIm(self):
+        return self.__im
+
     def __add__(self,other):
         return Complex(self.__re + other.__re, self.__im + other.__im)
 
@@ -13,9 +19,23 @@ class Complex:
         return Complex(self.__re / other.__re, self.__im / other.__im)
 
     def __abs__(self):
+        return Complex((self.__re**2)**(1/2),(self.__im**2)**(1/2))
 
     def __eq__(self, other):
         return Complex(self.__re == other.__re and self.__im == other.__im)
 
     def __ne__(self, other):
         return Complex(self.__re != other.__re)
+
+if __name__ == "__main__":
+    c1 = Complex(1,2)
+    c2 = Complex(2,1)
+    c3 = Complex(-3,5)
+    cadd = c1 + c2
+    csub = c1 - c2
+    cdiv = c1/c2
+    c3abs = abs(c3)
+    print("cadd(",str(cadd.getRe()),",",str(cadd.getIm()),")")
+    print("csub(",str(csub.getRe()),",",str(csub.getIm()),")")
+
+

cce0a021ea20979c05753f96a31d1df3aa115c24
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-03-25 08:59:33 +0100

    final

---
+++ Rational.py
@@ -0,0 +1,37 @@
+class Rational:
+    def __init__(self,num,denum):
+        self.__num = num
+        self.__denum = denum
+
+    def __str__(self):
+        return (f"resultat: {self.__num} / {self.__denum}")
+
+    def __add__(self, other):
+        if self.__denum == other.__denum:
+            return Rational(self.__num + other.__num,self.__denum)
+        else:
+            num = self.__num * other.__denum + other.__num * self.__denum
+            denum = self.__denum * other.__denum
+            return Rational(num,denum)
+
+    def __sub__(self, other):
+        if self.__denum == other.__denum:
+            return Rational(self.__num - other.__num,self.__denum)
+        else:
+            num = self.__num * other.__denum - other.__num * self.__denum
+            denum = self.__denum * other.__denum
+            return Rational(num,denum)
+
+    def __mul__(self, other):
+        return Rational(self.__num*other.__num,self.__denum*other.__denum)
+
+    def __truediv__(self, other):
+        return Rational(self.__num*other.__denum,self.__denum*other.__num)
+
+
+r1 = Rational(1,2)
+r2 = Rational(3,4)
+print(r1 + r2)
+print(r1 - r2)
+print(r1 * r2)
+print(r1 / r2)
